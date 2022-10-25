0f22e81ecc1fa14b74b6fc676cb969e5de74354a
scottzyang <scottzyang@gmail.com>
2022-10-25 12:14:22 -0500

    Import Flask library

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!) 
+
+# import the Flask library
+from flask import Flask
+# set app variable to start writing routes
+app = Flask(__name__)
+
