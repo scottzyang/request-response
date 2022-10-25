f22ee7ba4fd1ede0a0b5b4bf59f1412fd679a914
scottzyang <scottzyang@gmail.com>
2022-10-25 14:33:25 -0500

    Add multiply form route

--- app.py
+++ app.py
@@ -3,30 +3,56 @@
 
 # import the Flask library
 import random
-from flask import Flask
+from flask import Flask, request
 # set app variable to start writing routes
 app = Flask(__name__)
 
+# ----------------------------------------------------------------------------------------
 @app.route('/')
 def homepage():
     '''Shows a greeting to the user'''
     return 'Are you there, world? It\'s me, Ducky!'
 
+# ----------------------------------------------------------------------------------------
 @app.route('/animal/<users_animal>')
 def favorite_animal(users_animal):
     '''display a message to the user that changes based on their favorite animal'''
     return f'<h1>Wow, {users_animal} is my favorite animal, too!</h1>'
 
+# ----------------------------------------------------------------------------------------
 @app.route('/dessert/<users_dessert>')
 def desserts(users_dessert):
     '''display a message to user that changes based on favorite dessert'''
     return f'<h1>How did you know that I liked {users_dessert} too?</h1>'
 
+# ----------------------------------------------------------------------------------------
 @app.route('/madlibs/<adjective>/<noun>')
 def madlib(adjective, noun):
     '''displays funny story using adjective and noun'''
     return f'<h1>That is a really {adjective} {noun}</h1>'
 
+# ----------------------------------------------------------------------------------------
+formData = f"""
+    <form action="/multiplyformresults" method="GET">
+        Input first number:
+        <input type="text" name="firstnum">
+        <br>
+        Input second number:
+        <input type="text" name="secondnum">
+        <input type="submit" value="multiply!">
+    </form>
+    """
+@app.route('/multiplyform/')
+def multiply_form():
+    return formData
+
+@app.route('/multiplyformresults', methods=['GET'])
+def multiply_inputs():
+    first_num = request.args.get("firstnum")
+    second_num = request.args.get("secondnum")
+    return f"<h3>{first_num} times {second_num} is {int(first_num) * int(second_num)}</h3>"
+
+# -----------------------------------------------------------------------------------------
 @app.route('/multiply/<number1>/<number2>')
 def multiply(number1, number2):
     '''multiplies the two numbers provided and displays results'''
@@ -35,6 +61,7 @@ def multiply(number1, number2):
     else:
         return f'<h1>Invalid inputs. Please try again by entering 2 numbers!</h1>'
 
+# -----------------------------------------------------------------------------------------
 @app.route('/sayntimes/<word>/<n>')
 def sayntimes(word, n):
     '''repeats word a number of times'''
@@ -48,6 +75,7 @@ def sayntimes(word, n):
     else:
         return f'<h1>Invalid input. Please try again by entering a word and a number!</h1>'
 
+# ----------------------------------------------------------------------------------------
 @app.route('/dicegame')
 def dicegame():
     ''' determines winner based on random rolls'''
@@ -73,5 +101,6 @@ def dicegame():
     # tie
     return f'<h2>You both rolled {random_num}. It\'s a tie! Try again!</h2>'
 
+# ----------------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
