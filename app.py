b1971ea343b61eb4d24fca63bed85f33667836dd
scottzyang <scottzyang@gmail.com>
2022-10-25 13:20:09 -0500

    Add h1 tags to each return:

--- app.py
+++ app.py
@@ -9,30 +9,30 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     '''Shows a greeting to the user'''
-    return 'Are you there, world? It\'s me, Ducky'
+    return '<h1>Are you there, world? It\'s me, Ducky</h1>'
 
 @app.route('/animal/<users_animal>')
 def favorite_animal(users_animal):
     '''display a message to the user that changes based on their favorite animal'''
-    return f'Wow, {users_animal} is my favorite animal, too!'
+    return f'<h1>Wow, {users_animal} is my favorite animal, too!</h1>'
 
 @app.route('/dessert/<users_dessert>')
 def desserts(users_dessert):
     '''display a message to user that changes based on favorite dessert'''
-    return f'How did you know that I liked {users_dessert} too?'
+    return f'<h1>How did you know that I liked {users_dessert} too?</h1>'
 
 @app.route('/madlibs/<adjective>/<noun>')
 def madlib(adjective, noun):
     '''displays funny story using adjective and noun'''
-    return f'That is a really {adjective} {noun}'
+    return f'<h1>That is a really {adjective} {noun}</h1>'
 
 @app.route('/multiply/<number1>/<number2>')
 def multiply(number1, number2):
     '''multiplies the two numbers provided and displays results'''
     if number1.isdigit() and number2.isdigit(): 
-        return f'{number1} times {number2} is {int(number1) * int(number2)}'
+        return f'<h1>{number1} times {number2} is {int(number1) * int(number2)}</h1>'
     else:
-        return f'Invalid inputs. Please try again by entering 2 numbers!'
+        return f'<h1>Invalid inputs. Please try again by entering 2 numbers!</h1>'
 
 @app.route('/sayntimes/<word>/<n>')
 def sayntimes(word, n):
