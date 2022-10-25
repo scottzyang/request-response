582443e7dd0ac417bc071145fee1f9d676742500
scottzyang <scottzyang@gmail.com>
2022-10-25 12:29:01 -0500

    Add new route for desserts

--- app.py
+++ app.py
@@ -16,5 +16,10 @@ def favorite_animal(users_animal):
     '''display a message to the user that changes based on their favorite animal'''
     return f'Wow, {users_animal} is my favorite animal, too!'
 
+@app.route('/dessert/<users_dessert>')
+def desserts(users_dessert):
+    '''display a message to user that changes based on favorite dessert'''
+    return f'How did you know that I liked {users_dessert} too?'
+
 if __name__ == '__main__':
     app.run(debug=True)
